db2bdbd80cc1540812b22c03d7ea0a93807a0480
Mark Bond <markbond615@gmail.com>
2025-04-09 12:12:04 -0500

    Added Retailers component which displays retailers

--- scripts/flowers.js
+++ scripts/flowers.js
@@ -1,17 +1,18 @@
 export const DisplayFlowers = async () => {
     const flowers = await fetch("http://localhost:8088/flowers").then(res => res.json());
-
-    let flowersHTML = `<ul class="flower-list">`
-
-    const flowersArray = flowers.map((flower) => {
+    
+    const flowersHTML = flowers.map((flower) => {
         return `
-            <li class="flower-card">
-                <h3>${flower.commonName}</h3>
-                <p>Color: ${flower.color}</p>
-            </li>`
+            <div class="flower">
+                <header class="flower-name">
+                    <h2>${flower.commonName}</h2>
+                </header>
+                <section class="flower-color">
+                    Color: ${flower.color}
+                </section>
+            </div>
+        `
     }).join("");
 
-    flowersHTML += flowersArray + `</ul>`;
-    
     return flowersHTML;
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,17 @@
 import { DisplayFlowers } from "./flowers.js";
+import { DisplayRetailers } from "./retailers.js";
 
 const container = document.querySelector("#container");
 
 const render = async () => {
 
     const flowersHTML = await DisplayFlowers();
+    const retailersHTML = await DisplayRetailers();
 
     const composedHTML = 
     `<h2>Are You Feeling Bouquet?</h2>
         ${flowersHTML}
+        ${retailersHTML}
     `
     container.innerHTML = composedHTML;
 }
